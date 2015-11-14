5d804d66850368df06658301c233fef443f0ce5b
jacobpaine <painejacob@gmail.com>
2015-11-14 11:40:54 -0600

    Initial documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+Version 7.
+
+This is an extra effort to put together all the music-history elements we've learnt so far. 
+
+Coding work is focused on:
+
+	Bootstrap
+	Handlebars*
+	Require
+	Promises*
+
+* - likely issues
+
+Moqups and Trello will be used to make an attempt to learn practice project management.
+
+History:
+Problems with Moqups:
+Problems with Trello:
+
--- javascripts/musichistory.js
+++ javascripts/musichistory.js
@@ -81,56 +81,56 @@ var myFirebaseRef = new Firebase("https://fiery-inferno-6987.firebaseio.com/");
 		var selected_artists = $("#select-artists").find(":selected").text()
 		var selected_titles = $("#select-titles").find(":selected").text()
 	
-	if ($(this).attr("id") === "select-albums"){
-			for (var song in songList.songs){
-				if (songList.songs[song].album === selected_albums){
-					filtered.songs[song]=songList.songs[song];
-				}
-			}
-		
-		} else if ($(this).attr("id") === "select-artists"){
-			for (var song in songList.songs){
-				if (songList.songs[song].artist === selected_artists){
-					filtered.songs[song]=songList.songs[song];
+		if ($(this).attr("id") === "select-albums"){
+				for (var song in songList.songs){
+					if (songList.songs[song].album === selected_albums){
+						filtered.songs[song]=songList.songs[song];
+					}
 				}
 			
-			}
-		} else {
-			for (var song in songList.songs){
-				if (songList.songs[song].title === selected_titles){
-					filtered.songs[song]=songList.songs[song];	
+			} else if ($(this).attr("id") === "select-artists"){
+				for (var song in songList.songs){
+					if (songList.songs[song].artist === selected_artists){
+						filtered.songs[song]=songList.songs[song];
+					}
+				
+				}
+			} else {
+				for (var song in songList.songs){
+					if (songList.songs[song].title === selected_titles){
+						filtered.songs[song]=songList.songs[song];	
+					}
 				}
 			}
-		}
-        require(['hbs!../templates/songs'], function(songTemplate) {
-        	uniqueTitles = [];
-          $("#title-output").html(songTemplate(filtered));        
-        });
+	        require(['hbs!../templates/songs'], function(songTemplate) {
+	        	uniqueTitles = [];
+	          $("#title-output").html(songTemplate(filtered));        
+	        });
 
-        require(['hbs!../templates/albums'], function(songTemplate) {
-        	uniqueAlbums = [];
-          $("#album-output").html(songTemplate(filtered));
-            });
+	        require(['hbs!../templates/albums'], function(songTemplate) {
+	        	uniqueAlbums = [];
+	          $("#album-output").html(songTemplate(filtered));
+	            });
 
-        require(['hbs!../templates/artists'], function(songTemplate) {
-        	uniqueArtists = [];
-          $("#artist-output").html(songTemplate(filtered));
-            });
+	        require(['hbs!../templates/artists'], function(songTemplate) {
+	        	uniqueArtists = [];
+	          $("#artist-output").html(songTemplate(filtered));
+	            });
 
 	});
-// *****************ADD SONGS*************************
-	$(document).on("click", "#addSong", function(e){
-		var newSong = {
-			"title": $("#song--input").val(),
-			"artist": $("#artist--input").val(),
-			"album": $("#album--input").val()
-			}
-		
-	 	$.ajax({
-	 		url:"https://fiery-inferno-6987.firebaseio.com/songs/songs.json",
-	 		method: "POST", 
-	 		data: JSON.stringify(newSong)
-	 	}).done(function(addedSong) {
+	// *****************ADD SONGS*************************
+		$(document).on("click", "#addSong", function(e){
+			var newSong = {
+				"title": $("#song--input").val(),
+				"artist": $("#artist--input").val(),
+				"album": $("#album--input").val()
+				}
+			
+		 	$.ajax({
+		 		url:"https://fiery-inferno-6987.firebaseio.com/songs/songs.json",
+		 		method: "POST", 
+		 		data: JSON.stringify(newSong)
+		 	}).done(function(addedSong) {
  		});
  	
 
