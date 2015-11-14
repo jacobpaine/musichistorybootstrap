aeebf9e41adf0e99e988f21c881510b3e6908232
jacobpaine <painejacob@gmail.com>
2015-11-14 11:15:04 -0600

    Push long overdue

---
+++ javascripts/mh.js
@@ -0,0 +1,32 @@
+requirejs.config({
+  baseUrl: "./javascripts",
+  paths:{
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "q": '../bower_components/q/q'
+  },
+
+
+  shim: {
+    'bootstrap': {
+      deps: ['jquery'],
+      exports: 'bootstrap'
+      
+    },
+      "firebase": {
+        exports: "Firebase"
+      }
+  }
+});
+
+
+
+require(
+	["bootstrap", "hbs/handlebars", "musichistory", "populate-songs", "firebase"],
+	function(b, Handlebars, mh, pop, firebase) {
+
+	});
+
+
---
+++ javascripts/populate-songs.js
@@ -0,0 +1,51 @@
+define(["jquery", "firebase"], function($, Firebase) {
+  var myFirebaseRef = new Firebase("https://fiery-inferno-6987.firebaseio.com/songs");
+  myFirebaseRef.on("value", function(snapshot) {
+
+
+    var allSongsObject = snapshot.val();
+
+    templateStuffer(allSongsObject);
+
+    function templateStuffer (songs) {
+
+            songList = songs;
+
+            require(['hbs!../templates/albums'], function(songTemplate) {
+              $("#select-albums").html(songTemplate(songs));
+            });
+
+            require(['hbs!../templates/songs'], function(songTemplate) {
+              $("#select-titles").html(songTemplate(songs));
+            });
+
+            require(['hbs!../templates/artists'], function(songTemplate) {
+              $("#select-artists").html(songTemplate(songs));
+            });
+
+            require(['hbs!../templates/addform'], function(songTemplate) {
+              $("#add-here").html(songTemplate(songs));
+            });
+
+            require(['hbs!../templates/delete-song'], function(songTemplate) {
+              $("some-output").html(songTemplate());
+            });
+
+
+          }
+
+
+    });
+
+      // return {
+      //   getsongs: function(populate) {
+      //     $.ajax({
+      //     	url: "https://fiery-inferno-6987.firebaseio.com/.json"
+      //     }).done();
+      //   }
+      // };
+ });
+
+
+
+
